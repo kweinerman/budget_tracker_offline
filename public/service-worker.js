6e255647bb10c7a1af1248eb25647bf00ee23321
kweinerman <85956994+kweinerman@users.noreply.github.com>
2021-11-22 17:52:13 -0700

    Starter code

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@ const FILES_TO_CACHE = [
   self.addEventListener('install', (event) => {
     event.waitUntil(
       caches
-        .open(PRECACHE)
+        .open(CACHE_NAME)
         .then((cache) => cache.addAll(FILES_TO_CACHE))
         .then(self.skipWaiting())
     );
